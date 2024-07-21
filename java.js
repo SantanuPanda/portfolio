7be8e3ec824f45c701b5fea8e6e3cdf1e59f9e19
SANTANU PANDA <theultimate75399@gmail.com>
2024-07-21 20:19:31 +0530

    portfolio

---
+++ java.js
@@ -0,0 +1,10 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    let menuIcon = document.querySelector('#menu-icon');
+    let navbar = document.querySelector('.navbar');
+
+    menuIcon.onclick = () => {
+        menuIcon.classList.toggle('bx-x');
+        navbar.classList.toggle('active');
+    };
+});
